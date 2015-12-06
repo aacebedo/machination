0b053101f1fe1e60cc915a97d5da5d99527040c0
aacebedo <alexandre@acebedo.fr>
2015-12-06 10:56:03 +0100

    Validated pylint on constants.py

--- src/share/machination/python/machination/constants.py
+++ src/share/machination/python/machination/constants.py
@@ -1,4 +1,4 @@
-##########################################################################
+#
 # Machination
 # Copyright (c) 2014, Alexandre ACEBEDO, All rights reserved.
 #
@@ -14,31 +14,78 @@
 #
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library.
-##########################################################################
-
+#
+"""
+This file contains the constants used in the project
+"""
 import os
 from distutils.version import StrictVersion
 
 MACHINATION_VERSION = StrictVersion("1.0.0")
 
 # Path of the program files
-MACHINATION_INSTALLDIR = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)),"..",'..','..','..'))
-MACHINATION_DEFAULTTEMPLATESDIR = os.path.abspath(os.path.join(MACHINATION_INSTALLDIR,"etc","default","machination","templates"))
-MACHINATION_DEFAULTPROVISIONERSDIR = os.path.abspath(os.path.join(MACHINATION_INSTALLDIR,"etc","default","machination","provisioners"))
-MACHINATION_DEFAULTANSIBLEPLAYBOOKSDIR = os.path.join(MACHINATION_DEFAULTPROVISIONERSDIR,"ansible","playbooks")
-MACHINATION_DEFAULTANSIBLEROLESDIR = os.path.join(MACHINATION_DEFAULTPROVISIONERSDIR,"ansible","roles")
-MACHINATION_DEFAULTPACKERDIR = os.path.join(MACHINATION_INSTALLDIR,"share","machination","packer")
-MACHINATION_PYTHONDIR = os.path.join(MACHINATION_INSTALLDIR,"share","machination","python")
-MACHINATION_VBOXDIR = os.path.join(MACHINATION_INSTALLDIR,"share","machination","virtualbox")
-MACHINATION_VERSIONFILE = os.path.join(MACHINATION_INSTALLDIR,"share","machination","version")
+MACHINATION_INSTALLDIR = os.path.abspath(
+    os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                 "..",
+                 '..',
+                 '..',
+                 '..'))
+MACHINATION_DEFAULTTEMPLATESDIR = os.path.abspath(
+    os.path.join(MACHINATION_INSTALLDIR,
+                 "etc",
+                 "default",
+                 "machination",
+                 "templates"))
+MACHINATION_DEFAULTPROVISIONERSDIR = os.path.abspath(
+    os.path.join(MACHINATION_INSTALLDIR,
+                 "etc",
+                 "default",
+                 "machination",
+                 "provisioners"))
+MACHINATION_DEFAULTANSIBLEPLAYBOOKSDIR = os.path.join(
+    MACHINATION_DEFAULTPROVISIONERSDIR,
+    "ansible",
+    "playbooks")
+MACHINATION_DEFAULTANSIBLEROLESDIR = os.path.join(
+    MACHINATION_DEFAULTPROVISIONERSDIR,
+    "ansible",
+    "roles")
+MACHINATION_DEFAULTPACKERDIR = os.path.join(
+    MACHINATION_INSTALLDIR,
+    "share",
+    "machination",
+    "packer")
+MACHINATION_PYTHONDIR = os.path.join(
+    MACHINATION_INSTALLDIR,
+    "share",
+    "machination",
+    "python")
+MACHINATION_VBOXDIR = os.path.join(
+    MACHINATION_INSTALLDIR,
+    "share",
+    "machination",
+    "virtualbox")
+MACHINATION_VERSIONFILE = os.path.join(
+    MACHINATION_INSTALLDIR,
+    "share",
+    "machination",
+    "version")
 
 # User dir to store instances and templates
-MACHINATION_USERDIR = os.path.abspath(os.path.join(os.path.expanduser("~"),".machination"))
-MACHINATION_USERINSTANCESDIR = os.path.join(MACHINATION_USERDIR,"instances")
-MACHINATION_USERTEMPLATESDIR = os.path.join(MACHINATION_USERDIR,"templates")
-MACHINATION_USERPROVISIONERSDIR = os.path.join(MACHINATION_USERDIR,"provisioners")
-MACHINATION_USERANSIBLEPLAYBOOKSDIR = os.path.join(MACHINATION_USERPROVISIONERSDIR,"ansible","playbooks")
-MACHINATION_USERANSIBLEROLESDIR = os.path.join(MACHINATION_USERPROVISIONERSDIR,"ansible","roles")
+MACHINATION_USERDIR = os.path.abspath(
+    os.path.join(os.path.expanduser("~"), ".machination"))
+MACHINATION_USERINSTANCESDIR = os.path.join(MACHINATION_USERDIR, "instances")
+MACHINATION_USERTEMPLATESDIR = os.path.join(MACHINATION_USERDIR, "templates")
+MACHINATION_USERPROVISIONERSDIR = os.path.join(
+    MACHINATION_USERDIR, "provisioners")
+MACHINATION_USERANSIBLEPLAYBOOKSDIR = os.path.join(
+    MACHINATION_USERPROVISIONERSDIR,
+    "ansible",
+    "playbooks")
+MACHINATION_USERANSIBLEROLESDIR = os.path.join(
+    MACHINATION_USERPROVISIONERSDIR,
+    "ansible",
+    "roles")
 
-MACHINATION_CONFIGFILE_NAME="machine.config"
-MACHINATION_PACKERFILE_NAME="machine.packer"
+MACHINATION_CONFIGFILE_NAME = "machine.config"
+MACHINATION_PACKERFILE_NAME = "machine.packer"
